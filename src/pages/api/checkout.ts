c5784b1d18d404cb7e342f32b59b64a22782e99e
Diogo Soares <diogo.soares@solarview.com.br>
2022-09-08 13:40:16 -0300

    retirando o console.log do checkout.ts

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -6,7 +6,7 @@ export default async function handler(
   res: NextApiResponse,
 ) {
   const { priceId } = req.body
-  console.log('priceId', priceId)
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' })
   }
